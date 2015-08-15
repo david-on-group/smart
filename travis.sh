5e02e5c96a4a8dad7d56129fb8dd849bed5a1cec
david-on <david-on@users.noreply.github.com>
2015-08-15 12:15:01 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-echo $UNAME
-echo $CI_HOME
 ls -a
 mkdir _test
 cd _test
-git config --global user.email "${t_email}"
+git config --global user.email $UMAIL
 git config --global user.name $UNAME
-git clone https://david-on:4bd8d2183d703b41f79c32519d8bf32a242d2d87@github.com/david-on-group/dummy.git
+git clone https://david-on:$UTOKEN@github.com/david-on-group/dummy.git
 cd ..
 cp -r _test ujtest
 cd ujtest/dummy
-echo 'Hello, world.' >foo2.txt
+echo 'Hello, world.' >foo3.txt
 git add .
 git add -u .
 git commit -m "test"

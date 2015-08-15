250ef8e099189f7cc163c5849bd7d3a078e08259
david-on <87.bdavid+1@gmail.com>
2015-08-15 11:46:33 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -7,7 +7,7 @@ cd _test
 git clone https://david-on:${t}@github.com/david-on-group/dummy.git
 cd ..
 cp -r _test ujtest
-cd _ujtest/dummy
+cd ujtest/dummy
 echo 'Hello, world.' >foo.txt
 git add .
 git add -u .

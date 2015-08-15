4ef5c8e14dd22a4cc73e2ec1d77dd6b95c32fe1d
david-on <87.bdavid+1@gmail.com>
2015-08-15 11:59:04 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -4,6 +4,8 @@ echo $CI_HOME
 ls -a
 mkdir _test
 cd _test
+git config --global user.email ${t_email}
+git config --global user.name ${t_name}
 git clone https://david-on:4bd8d2183d703b41f79c32519d8bf32a242d2d87@github.com/david-on-group/dummy.git
 cd ..
 cp -r _test ujtest

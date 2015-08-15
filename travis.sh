efe538f5c3dd319be5fc148f8e717e48a4660859
david-on <87.bdavid+1@gmail.com>
2015-08-15 11:38:52 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -5,4 +5,11 @@ ls -a
 mkdir _test
 cd _test
 git clone https://david-on:${t}@github.com/david-on-group/dummy.git
-ls -a
+cd ..
+cp _test ujtest
+cd ujtest/dummy
+echo 'Hello, world.' >foo.txt
+git add .
+git add -u .
+git commit -m "test"
+git push

2b028628e2b5b987dbced2c62fd0f96248e4c7c9
david-on <87.bdavid+1@gmail.com>
2015-08-15 11:52:24 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ echo $CI_HOME
 ls -a
 mkdir _test
 cd _test
-git clone https://david-on:${t2}@github.com/david-on-group/dummy.git
+git clone https://david-on:4bd8d2183d703b41f79c32519d8bf32a242d2d87@github.com/david-on-group/dummy.git
 cd ..
 cp -r _test ujtest
 cd ujtest/dummy
